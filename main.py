90f35cda04c7dadede9f306c7a11f277a3b88238
willassad <willassad@icloud.com>
2020-11-29 19:39:43 -0500

    Added function

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     # print('Test')
+    print('Hello')
 
 
 # Press the green button in the gutter to run the script.
