0737ac7d57076113969d1aa7c7090bf64271eb00
willassad <willassad@icloud.com>
2020-11-29 22:21:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,21 @@ class ForestFireData:
     Attributes:
         - month: the month when the fire occurred
         - day: the day of the month the fire occurred
-        - FFMC: the Fine Fuel Moisture Code according to FWI system
-        - DMC: the Duff Moisture Code according to FWI system
-        - DC: the Drought Code according to FWI system
-        - ISI: the Initial Spread Index according to FWI system
-        - 
+        - ffmc: the Fine Fuel Moisture Code according to FWI system
+        - dmc: the Duff Moisture Code according to FWI system
+        - dc: the Drought Code according to FWI system
+        - isi: the Initial Spread Index according to FWI system
+        - temperature: the temperature of the area where the fire started
+        - relative_humidity: the humidity of the area where fire started
+        - wind: the amount of wind in the area where the fire started
+        - rain: the amount of rain in the area where the fire started
+        - area: the area of forest burned
 
     Representation Invariants:
-     -
+     - 0 <= self.month <= 12
+     - 0 <= self.day <= 31
+     - 0.0 <= self.ffmc <= 101.0
+     - 
 
     Sample Usage:
     >>>
