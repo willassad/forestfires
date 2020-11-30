7770534f1990d94dfa78aa3321e68c0c966b5242
jess-zhai <jessica.zhai@mail.utoronto.ca>
2020-11-29 19:39:58 -0500

    added to function

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     # print('Test')
+    print('hi from jessica')
 
 
 # Press the green button in the gutter to run the script.
