e7f820ba09b1040fcc1ccef48a237df1d74ea19d
willassad <willassad@icloud.com>
2020-11-29 22:17:56 -0500

    added dataset

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # Import dataset
 from dataclasses import dataclass
 
+
 @dataclass
 class ForestFireData:
-    """Description
+    """We describe a forest fire in the same format as forestfires.csv
+       where each forest fire has a month, day, FFMC, DMC, DC, ISI,
+       temperature, RH, wind, rain, and area.
 
     Attributes:
-        -
+        - month: the month when the fire occurred
+        - day: the day of the month the fire occurred
+        - FFMC: the Fine Fuel Moisture Code according to FWI system
+        - DMC: the Duff Moisture Code according to FWI system
+        - DC: the Drought Code according to FWI system
+        - ISI: the Initial Spread Index according to FWI system
+        - 
 
     Representation Invariants:
      -
