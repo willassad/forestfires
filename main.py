89da89bf95cdbd7ab7d95b0127529bad20be410f
willassad <willassad@icloud.com>
2020-11-30 19:47:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import dataset
 import csv
 from dataclasses import dataclass
+from datetime import datetime
 from typing import List
 
 months_dict = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,
@@ -42,13 +43,12 @@ class ForestFireData:
      - 0.0 <= self.area
 
     Sample Usage:
-    >>> oct_fire = ForestFireData(month=10, day=6, ffmc=90.6,dmc=43.7, dc=686.9,\
+    >>> oct_fire = ForestFireData(timestamp=datetime(2003, 10, 2), ffmc=90.6,dmc=43.7, dc=686.9,\
                                   isi=6.7, temperature=14.6, humidity=33, wind=1.3,\
                                   rain=0, area=0)
     """
 
-    month: int
-    day: int
+    timestamp: datetime.date
     ffmc: float
     dmc: float
     dc: float
@@ -60,7 +60,7 @@ class ForestFireData:
     area: float
 
 
-def process_csv(file_path: str) -> List[ForestFireData]:
+def process_forestfires(file_path: str) -> List[ForestFireData]:
     """Process a csv file into a list of ForestFireData"""
     with open(file_path) as file:
         reader = csv.reader(file)
@@ -88,7 +88,45 @@ def row_to_forest_data(row: List[str]) -> ForestFireData:
     month = months_dict[row[2]]
     day = days_dict[row[3]]
 
-    return ForestFireData(month=month, day=day, ffmc=float(row[4]), dmc=float(row[5]),
+    return ForestFireData(timestamp=datetime(2000, month, day), ffmc=float(row[4]), dmc=float(row[5]),
                           dc=float(row[6]), isi=float(row[7]), temperature=float(row[8]),
                           humidity=float(row[9]), wind=float(row[10]),
                           rain=float(row[11]), area=float(row[12]))
+
+
+@dataclass
+class PortugalTemperatureData:
+    """Docstring """
+    timestamp: datetime.date
+    city: str
+    average_temp: float
+    uncertainty: float
+
+
+def process_temperatures(file_path: str, city: str) -> List[PortugalTemperatureData]:
+    """Process a csv file into a list of PortugalTemperatureData"""
+    with open(file_path) as file:
+        reader = csv.reader(file)
+
+        data_so_far = []  # ACCUMULATOR: update list of data
+        for row in reader:
+            if city in row:
+                # process each row and add to data_so_far
+                data_so_far.append(row_to_temperature_data(row))
+
+    return data_so_far
+
+
+def row_to_temperature_data(row: List[str]) -> PortugalTemperatureData:
+    """ Convert a row of forestfires.csv into PortugalTemperatureData
+    """
+    time_data = [int(x) for x in row[0].split('-')]
+
+    return PortugalTemperatureData(timestamp=datetime(time_data[0], time_data[1],
+                                   time_data[2]), city=row[3], average_temp=float(row[1]),
+                                   uncertainty=float(row[2]))
+
+
+def main() -> None:
+    """ Main """
+    pass
