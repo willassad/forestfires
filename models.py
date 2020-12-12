aa8cf9dd76f042ff8dc0d0de9388b29e233a881f
jess-zhai <jessica.zhai@mail.utoronto.ca>
2020-12-12 16:12:42 -0500

    Update models.py
    changed function name 'plot_double_regression' to 'plot_variables' since the function doesn't do double regression;
    added instance attributes and preconditions and some examples; changed the return of function model_coef_double_regression from None to a tuple of 3 floats, the constant and the 2 coefficients.

--- models.py
+++ models.py
@@ -115,34 +115,65 @@ class Model:
         fig.show()
 
 
-def plot_double_regression(file_name: str, indep_var1: str, indep_var2: str, dep_var: str) -> None:
+def plot_variables(indep_var1: str, indep_var2: str, dep_var: str) -> None:
     """ Plot the scatter plot of dependent variable in a 3d graph with the 2 independent variables.
+    Instance Attributes:
+        - indep_var1: the name of the first independent variable for the double regression
+        - indep_var2: the name of the second independent variable for the double regression
+        - dep_var: the name of the dependent variable  for the double regression
+
+    Preconditions:
+        - indep_var1 in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - indep_var2 in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - dep_var in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
     """
-    data_col = process_fires_col(file_name)  # put the data into columns of each factor.
+    data_col = process_fires_col('forestfires.csv')  # put the data from datafile into list of columns of each factor.
     df = pd.DataFrame(data_col, columns=['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
                                          'wind', 'rain', 'area'])  # generate dataframe of the columns of factors
     fig = px.scatter_3d(df[[indep_var1, indep_var2, dep_var]], x=indep_var1, y=indep_var2, z=dep_var, opacity=0.6)
     # generate a 3d scatter plot, the x and y axis are value of the 2 independent variables, z-axis is the value for
     # the dependent variable.
-    fig.show()
+    fig.show()  # show the figure on website
 
 
-def plot_prediction_vs_outcome(file_name: str, dep_var: str, prediction: List[float]) -> None:
-    """ Plot the prediction calculated from regression vs the actual data from dataset
+def plot_prediction_vs_outcome(dep_var: str, prediction: List[float]) -> None:
+    """ Plot the prediction of a factor calculated from regression vs the actual values of that factor from datafile.
+    Instance Attributes:
+        - dep_var: the name of the one factor that affects forestfires that we are looking at
+        - predictions: the list of predicted value of that factor that is calculated from formula of double regression
+    Preconditions:
+        - dep_var in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - len(prediction) == len(process_forestfires('forestfires.csv'))
     """
-    data_col = process_fires_col(file_name)  # put the data into columns of each factor.
+    data_col = process_fires_col('forestfires.csv')  # put the data into columns of each factor.
     df = pd.DataFrame(data_col, columns=['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
                                          'wind', 'rain', 'area'])  # generate dataframe of the columns of factors
     df['prediction'] = prediction  # add the column of predictions, since it is not in data_col
     fig = px.scatter(df[[dep_var, 'prediction']], x=dep_var, y='prediction')  # generate scatter plot, original value
     # from the datafile on x-axis, prediction calculated from double regression on the y-axis
-    fig.show() # show the plot on website
+    fig.show()  # show the plot on website
 
 
-def calc_double_regression(file_name: str, y_0: float, b_1: float, b_2: float, x1: str, x2: str) -> List[float]:
+def calc_double_regression(y_0: float, b_1: float, b_2: float, x1: str, x2: str) -> List[float]:
     """ Calculate the value of dependent variable y using equaltion of double regression.
+    Instance Attributes:
+        - y_0: the constant
+        - b_1: the coefficient of the first variable
+        - b_2: the coefficient of the second variable
+        - x1: the name of the first variable
+        - x2: the name of the second variable
+    Preconditions:
+        - x1 in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - x2 in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - len(process_fires_col('forestfires.csv')[x1]) == len(process_fires_col('forestfires.csv')[x2])
     """
-    data_col = process_fires_col(file_name)  # get the data into dict of columns of each factor
+    data_col = process_fires_col('forestfires.csv')  # get the data into dict of columns of each factor
     x1_list = data_col[x1]  # get the column corresponding to x1
     x2_list = data_col[x2]  # get the column corresponding to x2
     y = []  # an empty list for the results of regression
@@ -153,17 +184,27 @@ def calc_double_regression(file_name: str, y_0: float, b_1: float, b_2: float, x
     return y  # return the list
 
 
-def model_coef_double_regression(file_name: str, indep_var1: str, indep_var2: str, dep_var: str) -> None:
+def model_coef_double_regression(indep_var1: str, indep_var2: str, dep_var: str) -> tuple:
     """ given 2 independent variable name and 1 dependent variable name, use statsmodel.OLS to model the
-        change of dependent variable due to changes of independent variables.
+        change of dependent variable due to changes of independent variables. Returns a tuple with 3 floats,
+        in the order of constant, coefficient of 1st independent variable, coefficient of 2nd independent
+        variable.
+    Instance Attributes:
+        - indep_var1: the name of the first independent variable
+        - indep_var2: the name of the second independent variable
+        - dep_var: the name of the dependent variable
+    Preconditions:
+        - indep_var1 in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - indep_var2 in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
+        - dep_var in ['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
+                   'wind', 'rain', 'area']
 
-        :param file_name: datafile we are using, the forestfire.csv file
-        :param indep_var1: the first independent variable
-        :param indep_var2: the second independent variable
-        :param dep_var: the dependent variable
-        :return: none
+    >>> model_coef_double_regression('forestfires.csv', 'ffmc', 'dc', 'temperature')
+    (-14.839067312278363, 0.31592664888750815, 0.009291464340286205)
     """
-    data_col = process_fires_col(file_name)  # put data into dict of columns of each factor
+    data_col = process_fires_col('forestfires.csv')  # put data into dict of columns of each factor
     df = pd.DataFrame(data_col, columns=['ffmc', 'dmc', 'dc', 'isi', 'temperature', 'humidity',
                                          'wind', 'rain', 'area'])  # generate dataframe from data_col
     x = df[[indep_var1, indep_var2]]  # x is the dataframe with only columns of the 2 independent variables
@@ -172,7 +213,9 @@ def model_coef_double_regression(file_name: str, indep_var1: str, indep_var2: st
 
     model = sm.OLS(y, x).fit()  # perform OSL on x and y, find the coefficients of the independent variables
     # that results in R squared closest to 1
-    print(model.summary())  # print the summary page of model
+    dict_model = dict(model.params)  # get the constant and the coefficients into a dictionary
+    return (dict_model['const'], dict_model[indep_var1], dict_model[indep_var2])  # return the values of the constant,
+    # the first coefficient, and the second coefficient as a tuple
 
 
 model = Model('forestfires.csv', 'portugaltemperatures.csv', 'annual_csv.txt', 'Braga')
