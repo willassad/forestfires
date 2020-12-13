821e16aa02ebdfd276f7758cb2d31aef041583e9
willassad <willassad@icloud.com>
2020-12-13 15:44:08 -0500

    Update models.py

--- models.py
+++ models.py
@@ -318,10 +318,8 @@ if __name__ == '__main__':
     })
 
     import python_ta.contracts
-
     python_ta.contracts.DEBUG_CONTRACTS = False
     python_ta.contracts.check_all_contracts()
 
     import doctest
-
     doctest.testmod()
